f0bb7c3675df6c04021863960ab25bff3ce66924
cgorrino <cgorrino03@gmail.com>
2021-06-15 22:16:46 -0700

    Adding route to delete a post, and routes for creating, login and logout of users.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -12,6 +12,24 @@ res.status(200).json(newPost);
     } catch(err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
