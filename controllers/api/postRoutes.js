947d7f32fa150f43a7cf9bac5916137072f4bf34
cgorrino <cgorrino03@gmail.com>
2021-06-15 21:43:46 -0700

    Adding route to create posts. Adding some styling to handlebars templates in the public folder.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,4 +2,16 @@ const router = require('express').Router();
 const {User, Post, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+const newPost = await Post.create({
+    ...req.body,
+    user_id: req.session.user_id,
+});
+res.status(200).json(newPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
